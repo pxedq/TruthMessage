20f96358ccdbc5bd91852863b3cbd2157f779146
pxedq <nagynandor2007online@gmail.com>
2026-01-06 13:35:03 +0100

    Added Firestore, Authentication and Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "/firebaseConfig.js";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import './App.css'
 
 export default function App() {
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  const auth = getAuth(app);
+
+  const router = createBrowserRouter([
+    { path: "/", element: <SignIn /> },
+    { path: "/", element: <SignUp /> },
+    { path: "/home", element: <Home /> },
+    { path: "/about", element: <About /> },
+    { path: "*", element: <Notfound /> }
+  ]);
+
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => setUser(currentUser));
+    return unsubscribe;
+  }, []);
+
   return (
     <div className='app'>
-      Hello World!
+      <RouterProvider router={router} />
     </div>
   )
 }
